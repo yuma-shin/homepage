61d4a79825e894cde3b5df57042644ad235a6efd
TsumTsumYuma <yuu7taku@icloud.com>
2023-01-03 23:16:38 +0900

    remove blog title

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -13,12 +13,14 @@ type Props = {
 export const Breadcrumbs = ({ tag }: Props) => {
     return (
         <Breadcrumb spacing='8px' separator={<ChevronRightIcon color='gray.500' fontSize="xl" fontWeight="bold" />} mb="8">
-            <BreadcrumbItem>
-                <BreadcrumbLink href='/blog' fontSize="2xl" fontWeight="bold" >Blog</BreadcrumbLink>
-            </BreadcrumbItem>
+            {
+                //<BreadcrumbItem>
+                //<BreadcrumbLink href='/blog' fontSize="2xl" fontWeight="bold" >Blog</BreadcrumbLink>
+                //</BreadcrumbItem>
+            }
             {tag && (
                 <BreadcrumbItem>
-                    <BreadcrumbLink href={`/tags/${tag.id}/page/1`} fontSize="2xl" fontWeight="bold">{tag.name}</BreadcrumbLink>
+                    <BreadcrumbLink href={`/tags/${tag.id}/page/1`} fontSize="2xl" fontWeight="bold">Tag : {tag.name}</BreadcrumbLink>
                 </BreadcrumbItem>
             )}
         </Breadcrumb>
